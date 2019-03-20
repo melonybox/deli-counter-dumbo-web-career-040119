24214f75843fa7ef3fdf82ae56b835a09179639e
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 04:00:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(list)
     newList = []
     list.each_with_index do |x,num|
       newList.push " #{num + 1}. #{x}"
+    end
     puts "The line is currently: #{newList}"
   else
     puts 'The line is currently empty.'
