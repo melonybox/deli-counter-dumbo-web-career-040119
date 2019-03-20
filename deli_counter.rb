aa20c2bc4d38fdbf59641aef43e4ad3e35d9cc76
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 04:03:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(list)
   if list.size > 0 
     newList = []
     list.each_with_index do |x,num|
-      newList.push " #{num + 1}. #{x}"
+      newList.push "#{num + 1}. #{x}"
     end
     puts "The line is currently:#{newList.join(" ")}"
   else
